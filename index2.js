babe15ba6273bb89aa7e6f5f4c010d19e167af96
ClaryNg <91236114+clarypom@users.noreply.github.com>
2022-07-18 11:42:31 -0400

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,143 @@
+//
+//
+// // Images Fade In
+//
+// const options = {
+//   threshold: 0.5,
+//   rootMargin: "0% 0% 5% 0%"
+// };
+//
+// const faders = document.querySelectorAll(".img");
+//
+// const scrollEffect = new IntersectionObserver(function(entries, scrollEffect){
+//   entries.forEach (entry => {
+//     if(!entry.isIntersecting){
+//       return;
+//     }
+//     entry.target.classList.add("fade-in");
+//   })
+// }, options);
+//
+// faders.forEach(fader => {
+//   scrollEffect.observe(fader);
+// });
+gsap.registerPlugin(ScrollTrigger);
+
+ScrollTrigger.create({
+  trigger: "iframe",
+  pin: true
+})
+
+gsap.to(".hello", {
+  y: -30,
+  scrollTrigger: {
+    trigger: ".hello",
+    start: "top 50%",
+    end: "top 10%",
+    scrub: true,
+    toggleClass: "ease-in",
+  }
+})
+
+gsap.to(".intro-interests", {
+  y: -30,
+  scrollTrigger: {
+    trigger: ".intro-interests",
+    start: "top 60%",
+    end: "top 20%",
+    scrub: true,
+    toggleClass: "ease-in"
+  }
+})
+
+gsap.to(".elaborate", {
+  y: -30,
+  scrollTrigger: {
+    trigger: ".elaborate",
+    start: "top 50%",
+    end: "top 10%",
+    scrub: true,
+    toggleClass: "ease-in"
+  }
+})
+
+gsap.to(".elaborate", {
+  y: -30,
+  scrollTrigger: {
+    trigger: ".elaborate",
+    start: "top 50%",
+    end: "top 10%",
+    scrub: true,
+    toggleClass: {targets: ".first-letter", className: "ease-in"}
+  }
+})
+
+ScrollTrigger.create({
+  trigger: "iframe",
+  scrub: true,
+  pin: false
+})
+
+// Images Fade In
+
+const options = {
+  threshold: 0.5,
+  rootMargin: "0% 0% 5% 0%"
+};
+
+const faders = document.querySelectorAll(".img");
+
+const scrollEffect = new IntersectionObserver(function(entries, scrollEffect){
+  entries.forEach (entry => {
+    if(!entry.isIntersecting){
+      return;
+    }
+    entry.target.classList.add("fade-in");
+  })
+}, options);
+
+faders.forEach(fader => {
+  scrollEffect.observe(fader);
+});
+
+gsap.utils.toArray(".img").forEach((img,i) =>{
+  ScrollTrigger.create({
+    trigger: img,
+    start: "top 40%",
+    end: "top 10%",
+    markers: true,
+    toggleClass: "fade-in"
+  })
+})
+
+gsap.utils.toArray(".text").forEach((text,i) => {
+  ScrollTrigger.create({
+    trigger: text,
+    start: "top 90%",
+    end: "top 20%",
+    toggleClass: "appear"
+  })
+})
+
+
+// ScrollTrigger.create({
+//   trigger: ".skills-projects",
+//   start: "top 50%",
+//   end: "top 100%",
+//   markers: true,
+//   scrub: true,
+//   toggleClass: {targets: ".ai-ml", className: "ease-in"}
+// })
+
+// gsap.utils.toArray(".layer").forEach((layer, i) => {
+//   ScrollTrigger.create({
+//     trigger: layer,
+//     start: "top top",
+//     pin: true,
+//     pinSpacing: false
+//   });
+// });
+//
+// ScrollTrigger.create({
+//   snap: 1/2
+// })
